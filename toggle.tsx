df9d3c335e7385d962f367fa7cd90b9bb29d52e8
Shreya Tiwari <shreyatiwari0801@gmail.com>
2026-05-25 12:39:17 +0530

    Update toggle.tsx

--- toggle.tsx
+++ toggle.tsx
@@ -10,7 +10,7 @@ const toggleVariants = cva(
     variants: {
       variant: {
         default: "bg-transparent",
-        outline: "border border-input bg-transparent hover:bg-accent hover:text-accent-foreground",
+        outline: "border border-input bg-transparent hover:bg-accent hover:text-accent-foreground", 
       },
       size: {
         default: "h-10 px-3",
@@ -35,3 +35,4 @@ const Toggle = React.forwardRef<
 Toggle.displayName = TogglePrimitive.Root.displayName;
 
 export { Toggle, toggleVariants };
+
